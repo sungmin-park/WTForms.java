b89663f37fc1614979bd25f509facaac945ab1fe
Sung Min Park <vamf12@gmail.com>
2014-08-16 23:29:40 +0900

    add static converts and widgets

--- src/main/java/kr/redo/wtforms/fields/TextAreaField.java
+++ src/main/java/kr/redo/wtforms/fields/TextAreaField.java
@@ -1,12 +1,12 @@
 package kr.redo.wtforms.fields;
 
 import kr.redo.wtforms.converters.StringConverter;
-import kr.redo.wtforms.widget.TextAreaWidgets;
+import kr.redo.wtforms.widget.TextAreaWidget;
 
 import static kr.redo.wtforms.converters.StringConverter.STRING_CONVERTER;
-import static kr.redo.wtforms.widget.TextAreaWidgets.TEXT_AREA_WIDGET;
+import static kr.redo.wtforms.widget.TextAreaWidget.TEXT_AREA_WIDGET;
 
-public class TextAreaField extends Field<String, StringConverter, TextAreaWidgets> {
+public class TextAreaField extends Field<String, StringConverter, TextAreaWidget> {
     public TextAreaField() {
         super(STRING_CONVERTER, TEXT_AREA_WIDGET);
     }
--- src/main/java/kr/redo/wtforms/widget/TextAreaWidget.java
+++ src/main/java/kr/redo/wtforms/widget/TextAreaWidget.java
@@ -6,12 +6,12 @@ import org.rendersnake.HtmlCanvas;
 
 import static org.rendersnake.HtmlAttributesFactory.id;
 
-public class TextAreaWidgets extends Widget {
+public class TextAreaWidget extends Widget {
     @NotNull
     @Override
     public String render(@NotNull String id, @NotNull String name, @Nullable String value) throws Exception {
         return new HtmlCanvas().textarea(id(id).name(name)).content(value).toHtml();
     }
 
-    public static final TextAreaWidgets TEXT_AREA_WIDGET = new TextAreaWidgets();
+    public static final TextAreaWidget TEXT_AREA_WIDGET = new TextAreaWidget();
 }
