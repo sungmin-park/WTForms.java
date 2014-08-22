02f4096b32ffdc2f954b61ad9fb2782e010ecbb1
Sung Min Park <vamf12@gmail.com>
2014-08-22 18:33:13 +0900

    add Options Field Test

--- src/main/java/kr/redo/wtforms/fields/OptionsField.java
+++ src/main/java/kr/redo/wtforms/fields/OptionsField.java
@@ -1,12 +1,30 @@
 package kr.redo.wtforms.fields;
 
-import sun.reflect.generics.reflectiveObjects.NotImplementedException;
-
 import javax.servlet.http.HttpServletRequest;
+import java.util.Optional;
 
 public class OptionsField extends AbstractField {
+    private Optional<String> value = Optional.empty();
+    private String[] options = {};
+
     @Override
     public void processData(HttpServletRequest request) {
-        throw new NotImplementedException();
+        final String parameter = request.getParameter(getParameterName());
+        if (parameter == null) {
+            return;
+        }
+        for (String option : options) {
+            if (option.equals(parameter)) {
+                value = Optional.of(parameter);
+            }
+        }
+    }
+
+    public Optional<String> getValue() {
+        return value;
+    }
+
+    public void setOptions(String[] options) {
+        this.options = options;
     }
 }
---
+++ src/test/java/kr/redo/wtforms/fields/OptionsFieldTest.java
@@ -0,0 +1,32 @@
+package kr.redo.wtforms.fields;
+
+import kr.redo.wtforms.forms.Form;
+import org.junit.Test;
+import org.springframework.mock.web.MockHttpServletRequest;
+
+import java.util.Optional;
+
+import static org.junit.Assert.*;
+
+public class OptionsFieldTest {
+    @Test
+    public void testProcessData() throws InstantiationException, IllegalAccessException {
+        final OptionsFieldTestForm form = Form.bind(OptionsFieldTestForm.class);
+        final OptionsField optionsField = form.getOptionsField();
+        final MockHttpServletRequest request = new MockHttpServletRequest();
+        request.addParameter("wtf-options-field", "value");
+        optionsField.processData(request);
+        assertEquals(Optional.<String>empty(), optionsField.getValue());
+        optionsField.setOptions(new String[]{"value"});
+        optionsField.processData(request);
+        assertEquals(Optional.of("value"), optionsField.getValue());
+    }
+
+    public static class OptionsFieldTestForm extends Form {
+        public OptionsField getOptionsField() {
+            return optionsField;
+        }
+
+        private OptionsField optionsField = new OptionsField();
+    }
+}
