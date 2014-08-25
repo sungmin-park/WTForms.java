e4b9617c30551f99e5584b2de702a02fa2155276
Sung Min Park <vamf12@gmail.com>
2014-08-26 08:03:51 +0900

    add java tuples and fix TextFielˆ

--- src/main/java/kr/redo/wtforms/fields/TextField.java
+++ src/main/java/kr/redo/wtforms/fields/TextField.java
@@ -1,16 +1,16 @@
 package kr.redo.wtforms.fields;
 
-import kr.redo.wtforms.transformers.Transformer;
+import kr.redo.wtforms.transformers.ValueTransformer;
 import kr.redo.wtforms.validators.Validator;
 
 import static kr.redo.wtforms.widgets.TextWidget.TEXT_WIDGET;
 
 public class TextField<T> extends Field<T> {
-    public TextField(Transformer<T> transformer) {
+    public TextField(ValueTransformer<T> transformer) {
         super(transformer, TEXT_WIDGET);
     }
 
-    public TextField(Transformer<T> transformer, Validator<T> validator) {
+    public TextField(ValueTransformer<T> transformer, Validator<T> validator) {
         super(transformer, TEXT_WIDGET, validator);
     }
 }
